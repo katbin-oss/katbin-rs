61f820a25175a90d97495cb0cbdeb8264db608e5
Sphericalkat <amolele@gmail.com>
2024-01-28 21:54:18 +0530

    feat: set up axum listener
    Signed-off-by: Sphericalkat <amolele@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+use axum::{routing::get, Router};
+
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init()
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
 }
