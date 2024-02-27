ce86a4a1932cfa65ca3e671e15ed750778863828
Sphericalkat <amolele@gmail.com>
2024-02-27 20:40:44 +0530

    feat: add db connection
    Signed-off-by: Sphericalkat <amolele@gmail.com>

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -2,11 +2,23 @@ use std::env;
 
 use anyhow::Ok;
 use axum::{routing::get, Router};
+use service::sea_orm::Database;
 
 #[tokio::main]
 async fn start() -> anyhow::Result<()> {
     env::set_var("RUST_LOG", "debug");
     tracing_subscriber::fmt::init();
+
+    // load env variables
+    dotenvy::dotenv()?;
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL not found in environment");
+    let port = env::var("PORT").expect("PORT not found in environment");
+
+    // make db connection
+    let conn = Database::connect(db_url)
+        .await
+        .expect("database connection failed");
+
 
     let app = Router::new().route("/", get(root));
 
